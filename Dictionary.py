b4fb70a749798f8110c4c7263dbac35e03072ba5
권강빈 <105830988+kwonkangbin@users.noreply.github.com>
2023-01-15 21:53:36 +0900

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#딕셔너리는 키와 밸류로만 되어있음
+# UTF-8 encoding when using korean
+dic1 = {"apple":"사과","bird":"새","bug":"벌레"}
+print(dic1)
+print(type(dic1))
+
+dic1 = {"apple":"과사"}
+print(dic1["apple"])
