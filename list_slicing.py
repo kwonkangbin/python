7132b5284913851bf0f010a1adb2c7cd50fb2e01
권강빈 <105830988+kwonkangbin@users.noreply.github.com>
2023-01-15 21:52:10 +0900

    Create list_slicing.py

---
+++ list_slicing.py
@@ -0,0 +1,14 @@
+odd = [1,3,5,7,9];
+cafes = ['star','bene','yoger','friends'];
+A = [1,5,'A','CC','B'];
+listIn = [[1,3,5,6,7],cafes,odd,1,3,'Abc'];
+
+a = odd[1:]
+b = cafes[1:];
+c=A[0:2];
+d = listIn[0][1:4];
+
+print(a)
+print(b)
+print(c)
+print(d)
