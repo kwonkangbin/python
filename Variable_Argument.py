0312c871684e2f98dec1cbdfa7779df0fb2334d8
권강빈 <105830988+kwonkangbin@users.noreply.github.com>
2023-01-16 22:11:07 +0900

    Create Variable_Argument.py

---
+++ Variable_Argument.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+def profile(name,age,lang1,lang2,lang3,lang4,lang5):
+	print("이름 : {0}\t나이 : {1}\t".format(name,age),end="")
+	print(lang1,lang2,lang3,lang4,lang5)
+
+profile("유재석",20,"python","JS","DART","JAVA","C")
+profile("강빈",10,"C++","JS","TS","JAVA","C")
